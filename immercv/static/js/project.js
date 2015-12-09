aafbb3332d26a0523206d60346091b47ef92fb6c
Matthew Scott <matt@11craft.com>
2015-12-09 16:56:53 -0600

    Add a preview button for auth'd users; speed up animations

--- immercv/static/js/project.js
+++ immercv/static/js/project.js
@@ -2,12 +2,23 @@
 
 $(function () {
 
-  $('.cvgraph-prop-editor form').hide();
-  $('.cvgraph-prop-editor button[data-property-editor=true]').click(function () {
+  var $editors = $('.cvgraph-editor');
+
+  $editors.find('form').hide();
+  $editors.find('button[data-property-editor=true]').click(function () {
     var $button = $(this);
     var $form = $button.parent().find('form');
     $button.toggleClass('active');
-    $form.toggle('visible');
+    $form.toggle(100);
+    if ($form.is(':visible')) {
+      $form.find('input[type=text],textarea').first().focus();
+    }
+  });
+
+  $('#preview-toggle').click(function () {
+    var $button = $(this);
+    $button.toggleClass('active');
+    $editors.toggle(100);
   });
 
 });
