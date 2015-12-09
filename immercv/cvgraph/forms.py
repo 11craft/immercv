6ce532e734c98b09e01b4270b3826a2dca375696
Matthew Scott <matt@11craft.com>
2015-12-09 15:19:26 -0600

    Support multiple properties in node property editor forms

--- immercv/cvgraph/forms.py
+++ immercv/cvgraph/forms.py
@@ -1,20 +1,31 @@
 from django import forms
-from neomodel import IntegerProperty, StringProperty
+from neomodel import DateProperty, IntegerProperty, StringProperty
+
+
+PROPERTY_TYPE_FIELDS = {
+    # property-type: form-field-type,
+    DateProperty: forms.DateField,
+    IntegerProperty: forms.IntegerField,
+    StringProperty: forms.CharField,
+}
 
 
 def field_for_node_property(node, property_name, **kwargs):
-    property_type = type(getattr(node.__class__, property_name))
-    return {
-        IntegerProperty: forms.IntegerField,
-        StringProperty: forms.CharField,
-    }[property_type](**kwargs)
+    property = getattr(node.__class__, property_name)
+    property_type = type(property)
+    kwargs['required'] = kwargs.pop('required', property.required)
+    return PROPERTY_TYPE_FIELDS[property_type](**kwargs)
 
 
-def form_for_node_property(node, property_name):
-    current_value = getattr(node, property_name)
+def form_for_node_properties(node, property_names, data=None):
+    current_values = {
+        property_name: getattr(node, property_name)
+        for property_name in property_names
+    }
     class NodeForm(forms.Form):
         def __init__(self, node, *args, **kwargs):
             super(NodeForm, self).__init__(*args, **kwargs)
-            self.fields[property_name] = field_for_node_property(
-                node, property_name)
-    return NodeForm(node, initial={property_name: current_value})
+            # Create fields in the order they're specified in `property_names`.
+            for property_name in property_names:
+                self.fields[property_name] = field_for_node_property(node, property_name)
+    return NodeForm(node, initial=current_values, data=data)
--- immercv/cvgraph/templatetags/cvgraph_tags.py
+++ immercv/cvgraph/templatetags/cvgraph_tags.py
@@ -1,14 +1,14 @@
 from django import template
 
-from immercv.cvgraph.forms import form_for_node_property
+from immercv.cvgraph.forms import form_for_node_properties
 from immercv.cvgraph.models import label_string
 
 register = template.Library()
 
 
 @register.inclusion_tag('cvgraph/tags/cvgraph_prop_editor.html')
-def cvgraph_prop_editor(node, property_name):
-    form = form_for_node_property(node, property_name)
+def cvgraph_prop_editor(node, *property_names):
+    form = form_for_node_properties(node, property_names)
     return {
         'labels': label_string(node.labels()),
         'form': form,
