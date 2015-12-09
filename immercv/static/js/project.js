9728ae2d2eab6f733a264ca45487b7a8c5b6890e
Matthew Scott <matt@11craft.com>
2015-12-09 15:20:13 -0600

    Hide edit forms by default; show pencil button to disclose them.

--- immercv/static/js/project.js
+++ immercv/static/js/project.js
@@ -1,1 +1,13 @@
 /* Project specific Javascript goes here. */
+
+$(function () {
+
+  $('.cvgraph-prop-editor form').hide();
+  $('.cvgraph-prop-editor button[data-property-editor=true]').click(function () {
+    var $button = $(this);
+    var $form = $button.parent().find('form');
+    $button.toggleClass('active');
+    $form.toggle('visible');
+  });
+
+});
