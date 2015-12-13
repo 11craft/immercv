cfd235bfc627e94ff8787da8c17593b6d3ba7acf
Matthew Scott <matt@11craft.com>
2015-12-13 15:14:06 -0600

    Change preview button to a checkbox; persist in a session cookie

--- immercv/static/js/project.js
+++ immercv/static/js/project.js
@@ -3,6 +3,7 @@
 $(function () {
 
   var $editors = $('.cvgraph-editor');
+  var $previewToggle = $('#preview-toggle');
 
   $editors.find('form').hide();
   $editors.find('button[data-property-editor=true]').click(function () {
@@ -15,10 +16,29 @@ $(function () {
     }
   });
 
-  $('#preview-toggle').click(function () {
-    var $button = $(this);
-    $button.toggleClass('active');
+  $previewToggle.click(function () {
     $editors.toggle(100);
+    if ($previewToggle.prop('checked') == true) {
+      document.cookie = 'preview=1; path=/';
+    } else {
+      document.cookie = 'preview=0; path=/';
+    }
   });
+
+  function getCookie(cname) {
+      var name = cname + "=";
+      var ca = document.cookie.split(';');
+      for(var i=0; i<ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0)==' ') c = c.substring(1);
+          if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+      }
+      return "";
+  }
+
+  if (getCookie('preview') == '1') {
+    $editors.toggle();
+    $previewToggle.prop('checked', true);
+  }
 
 });
