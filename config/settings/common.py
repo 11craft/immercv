694abf66fff9964dd1b64ad9b710811eb795bf3f
Matthew Scott <matt@11craft.com>
2016-01-05 17:48:31 -0600

    Enable Piwik analytics

--- config/settings/common.py
+++ config/settings/common.py
@@ -173,6 +173,7 @@ TEMPLATES = [
                 'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
                 # Your stuff: custom template context processors go here
+                'immercv.context_processors.analytics',
                 'immercv.context_processors.caching',
             ],
         },
@@ -243,3 +244,9 @@ AUTOSLUG_SLUGIFY_FUNCTION = 'slugify.slugify'
 ADMIN_URL = r'^admin/'
 
 # Your common stuff: Below this line define 3rd party library settings
+
+# ANALYTICS
+# ---------
+
+PIWIK_SERVER = env('PIWIK_SERVER')
+PIWIK_SITE_ID = env('PIWIK_SITE_ID')
--- immercv/context_processors.py
+++ immercv/context_processors.py
@@ -1,3 +1,13 @@
+from django.conf import settings
+
+
+def analytics(request):
+    return {
+        'PIWIK_SERVER': settings.PIWIK_SERVER,
+        'PIWIK_SITE_ID': settings.PIWIK_SITE_ID,
+    }
+
+
 def caching(request):
     return {
         'cache_timeout': 0 if request.user.is_authenticated() else 300,
